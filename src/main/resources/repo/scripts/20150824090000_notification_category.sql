ee1f31e9e5a8fa36566273c8e9b234654a5a1ed1
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-08-24 09:52:23 +0200

    Add category for notification system (refs #1585)

---
+++ src/main/resources/repo/scripts/20150824090000_notification_category.sql
@@ -0,0 +1,7 @@
+--New notification category 
+
+INSERT INTO `notification_category` (`last_update`, `code`, `name`, `bootstrap_glyphicon`) VALUES (NOW(), 'INFORMATION', 'Information', 'glyphicons glyphicons-circle-info');
+
+--//@UNDO
+
+
