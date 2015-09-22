f2125f81d710355c5ac98c42bf580319d187ebd8
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-09-22 10:48:10 +0200

    Add "md" in release burndown KPI (refs #1640)

--- src/main/resources/repo/scripts/20150915160000_kpi_release_burndown.sql
+++ src/main/resources/repo/scripts/20150915160000_kpi_release_burndown.sql
@@ -1,11 +1,11 @@
 --kpi
 
 INSERT INTO `kpi_value_definition` (`last_update`, `name`, `render_type`, `render_pattern`, `computation_js_code`, `is_trend_displayed`) 
-	VALUES (NOW(), 'kpi.release_burndown.main.name', 'VALUE', NULL, NULL, '1');
+	VALUES (NOW(), 'kpi.release_burndown.main.name', 'PATTERN', ':d md', NULL, '1');
 INSERT INTO `kpi_value_definition` (`last_update`, `name`, `render_type`, `render_pattern`, `computation_js_code`, `is_trend_displayed`) 
-	VALUES (NOW(), 'kpi.release_burndown.additional1.name', 'VALUE', NULL, NULL, '1');
+	VALUES (NOW(), 'kpi.release_burndown.additional1.name', 'PATTERN', ':d md', NULL, '1');
 INSERT INTO `kpi_value_definition` (`last_update`, `name`, `render_type`, `render_pattern`, `computation_js_code`, `is_trend_displayed`) 
-	VALUES (NOW(), 'kpi.release_burndown.additional2.name', 'VALUE', NULL, NULL, '0');
+	VALUES (NOW(), 'kpi.release_burndown.additional2.name', 'PATTERN', ':d md', NULL, '0');
 
 INSERT INTO `kpi_definition` (`last_update`, `uid`, `css_glyphicon`, `is_active`, `is_displayed`, `order`, `object_type`, `is_external`, `is_standard`, `clazz`, `scheduler_start_time`, `scheduler_frequency`, `scheduler_real_time`, `parameters`, `main_kpi_value_definition_id`, `additional1kpi_value_definition_id`, `additional2kpi_value_definition_id`) 
 	VALUES (NOW(), 'KPI_RELEASE_BURNDOWN', 'glyphicons glyphicons-scale-classic', '1', '1', '0', 'models.delivery.Release', '0', '1', 'services.kpi.ReleaseBurndownKpi', '03h00', '1440', '0', NULL, (SELECT id FROM kpi_value_definition WHERE name='kpi.release_burndown.main.name' LIMIT 1), (SELECT id FROM kpi_value_definition WHERE name='kpi.release_burndown.additional1.name' LIMIT 1), (SELECT id FROM kpi_value_definition WHERE name='kpi.release_burndown.additional2.name' LIMIT 1));
