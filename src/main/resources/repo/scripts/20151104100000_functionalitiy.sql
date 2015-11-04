6edb3b5b8046048f73f78c336e2c1886a7bacdea
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-11-04 09:41:24 +0100

    Adapt functionalities

---
+++ src/main/resources/repo/scripts/20151104100000_functionalitiy.sql
@@ -0,0 +1,11 @@
+UPDATE `system_permission` SET `selectable`='0' WHERE `name`='ADMIN_TRANSLATION_KEY_EDIT_PERMISSION';
+
+--//@UNDO
+
+UPDATE `system_permission` SET `selectable`='1' WHERE `name`='ADMIN_TRANSLATION_KEY_EDIT_PERMISSION';
+
+
+
+
+
+
