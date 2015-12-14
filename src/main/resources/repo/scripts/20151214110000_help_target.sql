d59a3efd2e19e47e306282ee4397b80ec486f2d1
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-12-14 16:32:26 +0100

    remove help link for sharpoint

--- src/main/resources/repo/scripts/20151214110000_help_target.sql
+++ src/main/resources/repo/scripts/20151214110000_help_target.sql
@@ -8,13 +8,13 @@ UPDATE help_target SET target='admin-guide:plugins:bizdock-internal-plugins:fina
 UPDATE help_target SET target='admin-guide:plugins:jira-plugin' WHERE route='/admin/plugin/definition/jira1';
 UPDATE help_target SET target='admin-guide:plugins:nexus-plugin-integrated' WHERE route='/admin/plugin/definition/nex1';
 UPDATE help_target SET target='admin-guide:plugins:bizdock-internal-plugins:bizdock-event-handler' WHERE route='/admin/plugin/definition/notification1';
-UPDATE help_target SET target='admin-guide:plugins' WHERE route='/admin/plugin/definition/sharp1';
 UPDATE help_target SET target='admin-guide:plugins:redmine-plugin' WHERE route='/admin/plugin/definition/redm2';
 
 INSERT INTO `help_target` (`last_update`, `route`, `target`) VALUES (NOW(), '/admin/plugin/definition/orgunitsload1', 'admin-guide:plugins:bizdock-internal-plugins:org-unit-loader-plugin');
 INSERT INTO `help_target` (`last_update`, `route`, `target`) VALUES (NOW(), '/admin/plugin/definition/genint1', 'admin-guide:plugins:bizdock-internal-plugins:bizdock-url-integration');
 INSERT INTO `help_target` (`last_update`, `route`, `target`) VALUES (NOW(), '/admin/plugin/definition/schedule1', 'admin-guide:plugins:bizdock-internal-plugins:bizdock-scheduled-script');
 
+DELETE FROM help_target WHERE route='/admin/plugin/definition/sharp1' LIMIT 1;
 
 --//@UNDO
 
