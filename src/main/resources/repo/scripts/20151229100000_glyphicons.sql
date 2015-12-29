8cb42996d43ad113749fb17f01e9534b04563585
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-12-29 09:29:12 +0100

    Replace glyphicons by font awesome (refs #TAF-264)

---
+++ src/main/resources/repo/scripts/20151229100000_glyphicons.sql
@@ -0,0 +1,35 @@
+--Replace glyphicons
+
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-check' WHERE bootstrap_glyphicon='glyphicons glyphicons-ok-2';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-bolt' WHERE bootstrap_glyphicon='glyphicons glyphicons-electricity';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-trash' WHERE bootstrap_glyphicon='glyphicons glyphicons-bin';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-envelope' WHERE bootstrap_glyphicon='glyphicons glyphicons-envelope';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-info-circle' WHERE bootstrap_glyphicon='glyphicons glyphicons-circle-info';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-bar-chart' WHERE bootstrap_glyphicon='glyphicons glyphicons-charts';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-paperclip' WHERE bootstrap_glyphicon='glyphicons glyphicons-paperclip';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-user' WHERE bootstrap_glyphicon='glyphicons glyphicons-parents';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-money' WHERE bootstrap_glyphicon='glyphicons glyphicons-coins';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-industry' WHERE bootstrap_glyphicon='glyphicons glyphicons-cargo';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-newspaper-o' WHERE bootstrap_glyphicon='glyphicons glyphicons-log-book';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-history' WHERE bootstrap_glyphicon='glyphicons glyphicons-history';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-code-fork' WHERE bootstrap_glyphicon='glyphicons glyphicons-git-branch';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-cogs' WHERE bootstrap_glyphicon='glyphicons glyphicons-cogwheels';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-university' WHERE bootstrap_glyphicon='glyphicons glyphicons-cluster';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-thumbs-up' WHERE bootstrap_glyphicon='glyphicons glyphicons-thumbs-up';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-cubes' WHERE bootstrap_glyphicon='glyphicons glyphicons-package';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-book' WHERE bootstrap_glyphicon='glyphicons glyphicons-address-book';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-inbox' WHERE bootstrap_glyphicon='glyphicons glyphicons-inbox';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-cloud' WHERE bootstrap_glyphicon='glyphicons glyphicons-cloud';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-clock-o' WHERE bootstrap_glyphicon='glyphicons glyphicons-clock';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-folder' WHERE bootstrap_glyphicon='glyphicons glyphicons-inbox-in';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-fire' WHERE bootstrap_glyphicon='glyphicons glyphicons-fire';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-sticky-note' WHERE bootstrap_glyphicon='glyphicons glyphicons-wallet';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-bug' WHERE bootstrap_glyphicon='glyphicons glyphicons-bug';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-calendar' WHERE bootstrap_glyphicon='glyphicons glyphicons-calendar';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-balance-scale' WHERE bootstrap_glyphicon='glyphicons glyphicons-scale-classic';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-tachometer' WHERE bootstrap_glyphicon='glyphicons glyphicons-scale';
+UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-folder' WHERE bootstrap_glyphicon='glyphicons glyphicons-sort';
+
+--//@UNDO
+
+
