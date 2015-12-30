c39ed3e65bd31f04642ebe239a99091401faa24f
jkohlertaf <johann.kohler@the-agile-factory.com>
2015-12-30 13:19:40 +0100

    Replace glyphicons by font awesome (refs #TAF-264)

--- src/main/resources/repo/scripts/20151229100000_glyphicons.sql
+++ src/main/resources/repo/scripts/20151229100000_glyphicons.sql
@@ -30,6 +30,46 @@ UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-balance-scale' 
 UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-tachometer' WHERE bootstrap_glyphicon='glyphicons glyphicons-scale';
 UPDATE portfolio_entry_event_type SET bootstrap_glyphicon='fa fa-folder' WHERE bootstrap_glyphicon='glyphicons glyphicons-sort';
 
+UPDATE kpi_definition SET css_glyphicon='fa fa-check' WHERE css_glyphicon='glyphicons glyphicons-ok-2';
+UPDATE kpi_definition SET css_glyphicon='fa fa-bolt' WHERE css_glyphicon='glyphicons glyphicons-electricity';
+UPDATE kpi_definition SET css_glyphicon='fa fa-trash' WHERE css_glyphicon='glyphicons glyphicons-bin';
+UPDATE kpi_definition SET css_glyphicon='fa fa-envelope' WHERE css_glyphicon='glyphicons glyphicons-envelope';
+UPDATE kpi_definition SET css_glyphicon='fa fa-info-circle' WHERE css_glyphicon='glyphicons glyphicons-circle-info';
+UPDATE kpi_definition SET css_glyphicon='fa fa-bar-chart' WHERE css_glyphicon='glyphicons glyphicons-charts';
+UPDATE kpi_definition SET css_glyphicon='fa fa-paperclip' WHERE css_glyphicon='glyphicons glyphicons-paperclip';
+UPDATE kpi_definition SET css_glyphicon='fa fa-user' WHERE css_glyphicon='glyphicons glyphicons-parents';
+UPDATE kpi_definition SET css_glyphicon='fa fa-money' WHERE css_glyphicon='glyphicons glyphicons-coins';
+UPDATE kpi_definition SET css_glyphicon='fa fa-industry' WHERE css_glyphicon='glyphicons glyphicons-cargo';
+UPDATE kpi_definition SET css_glyphicon='fa fa-newspaper-o' WHERE css_glyphicon='glyphicons glyphicons-log-book';
+UPDATE kpi_definition SET css_glyphicon='fa fa-history' WHERE css_glyphicon='glyphicons glyphicons-history';
+UPDATE kpi_definition SET css_glyphicon='fa fa-code-fork' WHERE css_glyphicon='glyphicons glyphicons-git-branch';
+UPDATE kpi_definition SET css_glyphicon='fa fa-cogs' WHERE css_glyphicon='glyphicons glyphicons-cogwheels';
+UPDATE kpi_definition SET css_glyphicon='fa fa-university' WHERE css_glyphicon='glyphicons glyphicons-cluster';
+UPDATE kpi_definition SET css_glyphicon='fa fa-thumbs-up' WHERE css_glyphicon='glyphicons glyphicons-thumbs-up';
+UPDATE kpi_definition SET css_glyphicon='fa fa-cubes' WHERE css_glyphicon='glyphicons glyphicons-package';
+UPDATE kpi_definition SET css_glyphicon='fa fa-book' WHERE css_glyphicon='glyphicons glyphicons-address-book';
+UPDATE kpi_definition SET css_glyphicon='fa fa-inbox' WHERE css_glyphicon='glyphicons glyphicons-inbox';
+UPDATE kpi_definition SET css_glyphicon='fa fa-cloud' WHERE css_glyphicon='glyphicons glyphicons-cloud';
+UPDATE kpi_definition SET css_glyphicon='fa fa-clock-o' WHERE css_glyphicon='glyphicons glyphicons-clock';
+UPDATE kpi_definition SET css_glyphicon='fa fa-folder' WHERE css_glyphicon='glyphicons glyphicons-inbox-in';
+UPDATE kpi_definition SET css_glyphicon='fa fa-fire' WHERE css_glyphicon='glyphicons glyphicons-fire';
+UPDATE kpi_definition SET css_glyphicon='fa fa-sticky-note' WHERE css_glyphicon='glyphicons glyphicons-wallet';
+UPDATE kpi_definition SET css_glyphicon='fa fa-bug' WHERE css_glyphicon='glyphicons glyphicons-bug';
+UPDATE kpi_definition SET css_glyphicon='fa fa-calendar' WHERE css_glyphicon='glyphicons glyphicons-calendar';
+UPDATE kpi_definition SET css_glyphicon='fa fa-balance-scale' WHERE css_glyphicon='glyphicons glyphicons-scale-classic';
+UPDATE kpi_definition SET css_glyphicon='fa fa-tachometer' WHERE css_glyphicon='glyphicons glyphicons-scale';
+UPDATE kpi_definition SET css_glyphicon='fa fa-folder' WHERE css_glyphicon='glyphicons glyphicons-sort';
+
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-user' WHERE bootstrap_glyphicon='glyphicons glyphicons-user';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-thumbs-up' WHERE bootstrap_glyphicon='glyphicons glyphicons-thumbs-up';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-exchange' WHERE bootstrap_glyphicon='glyphicons glyphicons-transfer';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-file-o' WHERE bootstrap_glyphicon='glyphicons glyphicons-file';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-bug' WHERE bootstrap_glyphicon='glyphicons glyphicons-bug';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-signal' WHERE bootstrap_glyphicon='glyphicons glyphicons-signal';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-sticky-note' WHERE bootstrap_glyphicon='glyphicons glyphicons-wallet';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-clock-o' WHERE bootstrap_glyphicon='glyphicons glyphicons-clock';
+UPDATE notification_category SET bootstrap_glyphicon='fa fa-info-circle' WHERE bootstrap_glyphicon='glyphicons glyphicons-circle-info';
+
 --//@UNDO
 
 
