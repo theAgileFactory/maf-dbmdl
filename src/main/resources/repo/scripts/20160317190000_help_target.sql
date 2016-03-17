2837556a63d4dce54c66605ba9b8c3554d6718a9
Marc Schaer <marc.schar@the-agile-factory.com>
2016-03-17 19:37:12 +0100

    add finances to help_target

---
+++ src/main/resources/repo/scripts/20160317190000_help_target.sql
@@ -0,0 +1,7 @@
+--Help target
+
+INSERT INTO `help_target` (`last_update`, `route`, `target`) VALUES (NOW(), '/admin/config/ref-data/finances', 'admin-guide:configuration:reference-data:finance');
+INSERT INTO `help_target` (`last_update`, `route`, `target`) VALUES (NOW(), '/admin/config/ref-data/finance/currency/manage', 'admin-guide:configuration:reference-data:finance');
+INSERT INTO `help_target` (`last_update`, `route`, `target`) VALUES (NOW(), '/admin/config/ref-data/finance/pe-budget-line-type/manage', 'admin-guide:configuration:reference-data:finance');
+
+--//@UNDO
