ff2f6f72c2c2924535642e50b6e5146d0644f79a
Guillaume Petit <guillaume.petit30@gmail.com>
2017-05-31 10:10:52 +0200

    #98 Add a permission to edit a portfolio entry as portfolio stakeholder

---
+++ src/main/resources/repo/scripts/20170530133456_v15-0-0_add_edit_as_portfolio_stakeholder_permission.sql
@@ -0,0 +1,22 @@
+-- // v15-0-0 add edit as portfolio stakeholder permission
+-- Migration SQL that makes the change goes here.
+
+INSERT INTO system_permission (name, description, deleted, selectable, last_update)
+VALUES (
+  'PORTFOLIO_ENTRY_EDIT_AS_PORTFOLIO_STAKEHOLDER_PERMISSION',
+  'permission.portfolio_entry_edit_as_portfolio_stakeholder_permission.description',
+  0,
+  1,
+  NOW()
+), (
+    'PORTFOLIO_ENTRY_EDIT_FINANCIAL_INFO_AS_PORTFOLIO_STAKEHOLDER_PERMISSION',
+    'permission.portfolio_entry_edit_as_financial_info_portfolio_stakeholder_permission.description',
+    0,
+    1,
+    NOW()
+  );
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DELETE FROM system_permission where name in ('PORTFOLIO_ENTRY_EDIT_AS_PORTFOLIO_STAKEHOLDER_PERMISSION', 'PORTFOLIO_ENTRY_EDIT_FINANCIAL_INFO_AS_PORTFOLIO_STAKEHOLDER_PERMISSION');
