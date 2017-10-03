65a5307e487558d973d28807d36d0b4eed80a8c8
Guillaume Petit <guillaume.petit30@gmail.com>
2017-10-03 15:42:09 +0200

    Fix locale for reports

--- src/main/resources/repo/scripts/20170828142004_v16-0-0_Change_french_locale.sql
+++ src/main/resources/repo/scripts/20170828142004_v16-0-0_Change_french_locale.sql
@@ -5,9 +5,15 @@ UPDATE i18n_messages
 SET language = 'fr-CH'
 WHERE language = 'fr';
 
+UPDATE reporting
+SET languages = replace(languages, 'fr', 'fr-CH');
+
 -- //@UNDO
 -- SQL to undo the change goes here.
 
 UPDATE i18n_messages
 SET language = 'fr'
 WHERE language = 'fr-CH';
+
+UPDATE reporting
+SET languages = replace(languages, 'fr-CH', 'fr');
