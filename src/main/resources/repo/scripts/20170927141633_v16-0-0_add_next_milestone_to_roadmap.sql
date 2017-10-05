447f9bcf4852d7463e6817c777900ebe7cfe93e5
Guillaume Petit <guillaume.petit30@gmail.com>
2017-10-05 16:19:19 +0200

    #129 Fix next milestone initialization

--- src/main/resources/repo/scripts/20170927141633_v16-0-0_add_next_milestone_to_roadmap.sql
+++ src/main/resources/repo/scripts/20170927141633_v16-0-0_add_next_milestone_to_roadmap.sql
@@ -8,17 +8,35 @@ ALTER TABLE `portfolio_entry`
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
 
-update
+UPDATE
     portfolio_entry pe
-    left join life_cycle_instance lci on pe.active_life_cycle_instance_id = lci.id
-    left join life_cycle_instance_planning lcip on lci.id = lcip.life_cycle_instance_id and lcip.deleted = 0 and lcip.is_frozen = 0
-    left join planned_life_cycle_milestone_instance plcmi on lcip.id = plcmi.life_cycle_instance_planning_id and plcmi.deleted = 0 and plcmi.id = (
-      select  min(id)
-      from    planned_life_cycle_milestone_instance
-      where   life_cycle_instance_planning_id = lcip.id
-    )
-set
-  pe.next_planned_life_cycle_milestone_instance_id = plcmi.id;
+    LEFT JOIN life_cycle_instance lci ON pe.active_life_cycle_instance_id = lci.id
+    LEFT JOIN life_cycle_instance_planning lcip
+      ON lci.id = lcip.life_cycle_instance_id AND lcip.deleted = 0 AND lcip.is_frozen = 0
+    LEFT JOIN planned_life_cycle_milestone_instance plcmi
+      ON lcip.id = plcmi.life_cycle_instance_planning_id AND plcmi.deleted = 0
+    LEFT JOIN life_cycle_milestone lcm ON plcmi.life_cycle_milestone_id = lcm.id
+    LEFT JOIN i18n_messages lcm_name ON lcm_name.`key` = lcm.short_name AND lcm_name.language = 'fr'
+    LEFT JOIN (
+                SELECT
+                  pe.id,
+                  min(lcm.`order`) AS min
+                FROM
+                  portfolio_entry pe
+                  LEFT JOIN life_cycle_instance lci ON pe.active_life_cycle_instance_id = lci.id
+                  LEFT JOIN life_cycle_instance_planning lcip
+                    ON lci.id = lcip.life_cycle_instance_id AND lcip.deleted = 0 AND lcip.is_frozen = 0
+                  LEFT JOIN planned_life_cycle_milestone_instance plcmi
+                    ON lcip.id = plcmi.life_cycle_instance_planning_id AND plcmi.deleted = 0
+                  LEFT JOIN life_cycle_milestone lcm ON plcmi.life_cycle_milestone_id = lcm.id
+                  LEFT JOIN i18n_messages lcm_name ON lcm_name.`key` = lcm.short_name AND lcm_name.language = 'fr'
+                GROUP BY
+                  pe.id
+
+              ) AS minOrder ON minOrder.id = pe.id
+SET
+  pe.next_planned_life_cycle_milestone_instance_id = plcmi.id
+WHERE minOrder.min = lcm.`order`;
 
 -- //@UNDO
 -- SQL to undo the change goes here.
